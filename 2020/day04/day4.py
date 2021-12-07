5079d9361b408ac161d6a315348ebde0261dcf54
Jesse Clark <jjclark1982@gmail.com>
2021-12-06 22:18:06 -0800

    Update 2020 files

--- 2020/day04/day4.py
+++ 2020/day04/day4.py
@@ -17,18 +17,18 @@ optional_fields = set([
 permitted_fields = required_fields | optional_fields
 
 def validate_hgt(hgt):
-	'''
-	hgt (Height) - a number followed by either cm or in:
-	    If cm, the number must be at least 150 and at most 193.
-	    If in, the number must be at least 59 and at most 76.
-	'''
-	if hgt.endswith('cm'):
-		val = int(hgt[:-2])
-		return 150 <= val <= 193
-	if hgt.endswith('in'):
-		val = int(hgt[:-2])
-		return 59 <= val <= 76
-	return False
+    '''
+    hgt (Height) - a number followed by either cm or in:
+        If cm, the number must be at least 150 and at most 193.
+        If in, the number must be at least 59 and at most 76.
+    '''
+    if hgt.endswith('cm'):
+        val = int(hgt[:-2])
+        return 150 <= val <= 193
+    if hgt.endswith('in'):
+        val = int(hgt[:-2])
+        return 59 <= val <= 76
+    return False
 
 validators = {
     'byr': lambda x: 1920 <= int(x) <= 2002, #(Birth Year) - four digits; at least 1920 and at most 2002.
@@ -38,7 +38,7 @@ validators = {
     'hcl': lambda x: re.match('^#[0-9a-f]{6}',x) is not None, # (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
     'ecl': lambda x: x in ['amb','blu','brn','gry','grn','hzl','oth'], #(Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
     'pid': lambda x: re.match('^[0-9]{9}$',x) is not None, #(Passport ID) - a nine-digit number, including leading zeroes.
-    'cid': lambda x: True #(Country ID) - ignored, missing or not.	
+    'cid': lambda x: True #(Country ID) - ignored, missing or not.  
 }
 
 
@@ -46,28 +46,28 @@ current_record = {}
 records = [current_record]
 
 with open('input.txt') as f:
-	for line in f:
-		line = line.strip()
-		if len(line) == 0:
-			current_record = {}
-			records.append(current_record)
-			continue
+    for line in f:
+        line = line.strip()
+        if len(line) == 0:
+            current_record = {}
+            records.append(current_record)
+            continue
 
-		fields = line.split()
-		for field in fields:
-			(name, value) = field.split(':')
-			current_record[name] = value
+        fields = line.split()
+        for field in fields:
+            (name, value) = field.split(':')
+            current_record[name] = value
 
 def is_valid(record):
-	fields = set(record.keys())
-	if fields & required_fields != required_fields:
-		return False
-	for field, value in record.items():
-		if field not in permitted_fields:
-			return False
-		if not validators[field](value):
-			return False
-	return True
+    fields = set(record.keys())
+    if fields & required_fields != required_fields:
+        return False
+    for field, value in record.items():
+        if field not in permitted_fields:
+            return False
+        if not validators[field](value):
+            return False
+    return True
 
 valid_records = [*filter(is_valid, records)]
 print("Valid records:", len(valid_records))
--- 2020/day05/day5.py
+++ 2020/day05/day5.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
 
-import re
 import numpy as np
 
+
 def get_row(line):
-	bits = np.array(list(line[0:7])) == 'B'
-	row = np.packbits(np.flip(bits), bitorder='little')[0]
-	return row
+    bits = np.array(list(line[0:7])) == 'B'
+    row = np.packbits(np.flip(bits), bitorder='little')[0]
+    return row
+
 
 def get_col(line):
-	bits = np.array(list(line[7:10])) == 'R'
-	col = np.packbits(np.flip(bits), bitorder='little')[0]
-	return col
+    bits = np.array(list(line[7:10])) == 'R'
+    col = np.packbits(np.flip(bits), bitorder='little')[0]
+    return col
 
-def get_sid(row, col):
-	return row * 8 + col
+
+def get_seat_id(row, col):
+    return row * 8 + col
 
 # print(get_row('BFFFBBFRRR'))
 # print(get_col('BFFFBBFRRR'))
-# print(get_sid(70,7))
+# print(get_seat_id(70,7))
 
 
 seats = []
 
 with open('input.txt') as f:
-	for line in f:
-		line = line.strip()
-		row = get_row(line)
-		col = get_col(line)
-		sid = get_sid(row, col)
-		seats.append((line, row, col, sid))
-		# print("row={}, col={}, sid={}".format(row,col,sid))
+    for line in f:
+        line = line.strip()
+        row = get_row(line)
+        col = get_col(line)
+        seat_id = get_seat_id(row, col)
+        seats.append((line, row, col, seat_id))
+        # print("row={}, col={}, seat_id={}".format(row,col,seat_id))
 
 seat_ids = [s[3] for s in seats]
 seat_ids = sorted(seat_ids)
