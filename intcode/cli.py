b592adacee47027d849d46a024f5f2c89c9888b8
Jesse Clark <jjclark1982@gmail.com>
2022-12-05 14:22:52 -0800

    create CLI

---
+++ intcode/cli.py
@@ -0,0 +1,29 @@
+from .intcode import Intcode
+
+
+def main():
+    import sys
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('program_file', type=argparse.FileType('r'))
+    args = parser.parse_args()
+
+    program = [*map(int, args.program_file.read().strip().split(','))]
+    args.program_file.close()
+
+    machine = Intcode(program)
+
+    # while sys.stdin.readable():
+    #     line = sys.stdin.readline()
+    #     for value in line.strip().split(','):
+    #         if value:
+    #             machine.input.put(value)
+    #         else:
+    #             break
+
+    output = machine.run()
+    print(''.join([*map(chr, output)]))
+
+
+if __name__ == '__main__':
+    main()
